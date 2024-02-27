d0c540ce5b932f4122785963a63ebb1a37fff0af
francogarcia02 <francoalbertogarcia2017@gmail.com>
2024-02-26 21:34:44 -0300

    Carrito terminado, solo pasarela de pago faltante

--- src/components/CartList/CartItem/CartItem.jsx
+++ src/components/CartList/CartItem/CartItem.jsx
@@ -1,12 +1,24 @@
-const CartItem = ({prod, remove}) => {
+import {PlusCircleFill, DashCircleFill, XCircleFill} from 'react-bootstrap-icons';
 
+const CartItem = ({prod, remove, remove_all, add}) => {
     const {name, price, quantity, id} = prod
+    const total = (price * quantity)
+
     return(
-        <div className="bg-dark d-flex text-light m-2 p-3 rounded">
-            <h5>{name}</h5>
-            <h5>total: ${price}</h5>
-            <h5>{quantity}</h5>
-            <button onClick={() => remove(id, false)}>remove</button>
+        <div className="bg-dark d-flex text-light m-2 p-3 rounded justify-content-between">
+            <h4>{name}</h4>
+            <div>
+                <h5>Precio individual: {price}</h5>
+                <h5>Total: ${total}</h5>
+            </div>
+            <div className="d-flex align-items-center">
+                <DashCircleFill id="plus-minus_button" onClick={() => remove(id)}/>
+                <p className="m-0 p-2">{quantity}</p>
+                <PlusCircleFill id="plus-minus_button" onClick={() => add(id)}/>
+            </div>
+            <div className="d-flex align-items-center">
+                <XCircleFill id="plus-minus_button" onClick={() => remove_all(id)}/>
+            </div>
         </div>
     )
 }
--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -1,5 +1,5 @@
 import {useSelector, useDispatch} from 'react-redux';
-import {remove, clear} from '../../actions/cartActions';
+import {remove, clear, add} from '../../actions/cartActions';
 import CartItem from './CartItem/CartItem';
 
 const CartList = () => {
@@ -10,9 +10,25 @@ const CartList = () => {
 
     return(
         <div className="d-grid">
-            {cart && cart.map(prod => (
-                <CartItem prod={prod} remove={() => dispatch(remove(prod.id, true))}/>
-            ))}
+            {cart.length >= 1 ?
+                <>
+                {cart.map(prod => (
+                    <CartItem
+                      prod={prod}
+                      remove={() => dispatch(remove(prod.id, false))}
+                      remove_all={() => dispatch(remove(prod.id, true))}
+                      add={() => dispatch(add(prod.id))}
+                     />
+                ))}
+                <div>
+                    <button onClick={()=>dispatch(clear())}>Borrar</button>
+                </div>
+                </>
+                :
+                <div className="bg-dark d-flex text-light m-2 p-3 rounded justify-content-center">
+                    <h3>No hay productos en el carrito</h3>
+                </div>
+            }
         </div>
     )
 }
