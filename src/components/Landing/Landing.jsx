972d751fc8c7a0f05b4221f74c21a2bf7a89e28b
francogarcia02 <francoalbertogarcia2017@gmail.com>
2024-02-29 22:27:43 -0300

    boton adicional

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,14 +1,13 @@
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import React, {useState} from 'react';
-import {add} from '../../actions/cartActions';
 import {BsChevronCompactLeft , BsChevronCompactRight} from 'react-icons/bs';
+import {Link} from 'react-router-dom';
 
 
 const Landing = () =>{
     const state = useSelector(state => state)
     const {carrito} = state
     const {products} = carrito
-    const dispatch = useDispatch()
 
     const [currentIndex, setCurrentIndex] = useState(0)
 
@@ -29,11 +28,12 @@ const Landing = () =>{
     }
 
     return(
-        <div className="p-3 text-indigo-600">
+        <div className="p-3 text-indigo-600 container">
             <h3 className="m-0 text-center">Nuestros productos principales</h3>
             {products && products[currentIndex] &&
             <div className="max-w-[50rem] m-auto py-16 p-5 relative group">
-                <img src={products[currentIndex].img} className="shadow rounded-2xl l-img bg-center bg-cover duration-500" alt="img"/>
+                <img src={products[currentIndex].img} className="shadow rounded-2xl w-full max-h-[400px] h-auto bg-center bg-cover duration-500" alt="img"/>
+                <Link to="/products" className="hidden group-hover:block btn btn-primary absolute top-[80%] right-[10%]">Ver mas</Link>
                 <div onClick={prevIndex} className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full bg-white/20 text-black cursor-pointer">
                     <BsChevronCompactLeft size={30}/>
                 </div>
